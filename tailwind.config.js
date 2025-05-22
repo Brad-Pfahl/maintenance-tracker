620f9c21029ab7ab696b2b2e0dd6800c9425bf9d
bpfahl <bpfahl008@gmail.com>
2025-05-21 20:46:54 -0400

    Color and theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     './assets/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#64748b', // slate-500
+          light: '#cbd5e1',   // slate-200
+          dark: '#475569',    // slate-600
+        },
+        background: {
+          DEFAULT: '#f8fafc', // slate-50
+        },
+        text: {
+          DEFAULT: '#334155', // slate-700
+          muted: '#64748b',   // slate-500
+        },
+        accent: {
+          DEFAULT: '#60a5fa', // blue-400
+          dark: '#3b82f6',    // blue-500
+        },
+        error: {
+          DEFAULT: '#b91c1c',
+          light: '#fee2e2',
+        },
+      },
+    }
+
   },
   plugins: [],
 };
